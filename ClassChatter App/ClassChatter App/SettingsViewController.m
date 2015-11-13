0645db30a7810d174ddfd71f02cf857c21b5af02
Adam Goldberg <adammgoldberg@hotmail.com>
2015-11-13 14:00:46 -0500

    ui changes

--- ClassChatter App/ClassChatter App/SettingsViewController.m
+++ ClassChatter App/ClassChatter App/SettingsViewController.m
@@ -79,8 +79,8 @@
     [self.informationView addSubview:scrollView];
     
     
-    UILabel *informationLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.informationView.bounds.origin.x + 20, self.informationView.bounds.origin.y + 40, self.informationView.bounds.size.width - 40, self.informationView.bounds.size.height + 680)];
-    informationLabel.numberOfLines = 300;
+    UILabel *informationLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.informationView.bounds.origin.x + 20, self.informationView.bounds.origin.y + 20, self.informationView.bounds.size.width - 40, self.informationView.bounds.size.height + 680)];
+    informationLabel.numberOfLines = 270;
     informationLabel.backgroundColor = [UIColor whiteColor];
     informationLabel.textColor = [UIColor colorWithRed:96/255.0f green:174/255.0f blue:82/255.0f alpha:1];
     informationLabel.text = @"Welcome to ClassTrack! Our aim is to help save you time as a teacher!\n\nBelow is information to help you use the app.\n\n1. Sending emails:\nIf you want to send an email to a parent, simply swipe up (represents good behaviour in class) or swipe down (represents poor behaviour in class) on a 'student desk' found in the Classroom page.\n\n2. Adding Students and Classes:\nIf you want to add a student or a class, simply press the 'Add Student' button where you simply will add in student, class, and parent information. The app uses this information to automatically fill in emails and save you time! This information is kept secure and private on the app and stored on your phone.\n\n3. History:\nAnytime you swipe up or down, the behaviour is recorded and stored on the history page. You can click on the behaviour event and add notes to it for future refrence.\n\n4. Templates:\nIn the Templates section, you can customize the positive and negative email templates that you send to parents. The placeholders will be automatically populated with the information you provided for each student and parent.\n\n5. Settings:\nHere you can customize the amount of 'good swipes' and 'bad swipes' it takes before an email is automatically populated and prepared to send. You can select a number, or turn off the ability to send emails and simply use the app to record behaviour in class. You can also put in your Principal's email address if you would like him or her to be CC'd on the emails that are sent out.\n\n6. Your Email Account:\nThe app works in conjunction with your phone's Mail App. If you have multiple email accounts in your mail app, you can select which account is sending the emails. Simply go to 'Settings' -> 'Mail, Contacts, Calendars' -> 'Default Account' and select which email account you want the app to use.";
@@ -88,9 +88,10 @@
     
     [scrollView addSubview:informationLabel];
     
-    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(self.informationView.bounds.origin.x + 20, self.informationView.bounds.origin.y + 15, 20, 20)];
+    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(self.informationView.bounds.origin.x + 20, self.informationView.bounds.origin.y + 35, 20, 20)];
     closeButton.backgroundColor = [UIColor colorWithRed:96/255.0f green:174/255.0f blue:82/255.0f alpha:1];
     [closeButton setTitle:@"X" forState:UIControlStateNormal];
+    closeButton.titleLabel.font = [UIFont systemFontOfSize:24.0];
     [closeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [closeButton addTarget:self action:@selector(removeInformation:) forControlEvents:UIControlEventTouchUpInside];
     
